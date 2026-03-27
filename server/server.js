730f67a5b09bcb639872f607c76dba5ceea32158
Eliannie <elianniematos@gmail.com>
2026-03-27 12:36:50 -0400

    Auth working: connected MongoDB, added auth routes, login/register and protected profile route

--- server/server.js
+++ server/server.js
@@ -1,16 +1,27 @@
 import express from "express";
-import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 import movieRoutes from "./routes/movieRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+
+dotenv.config();
 
 const app = express();
+
+connectDB();
+
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/movieapp")
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log(err));
+// test
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
 
 app.use("/movies", movieRoutes);
+app.use("/api/auth", authRoutes);
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
